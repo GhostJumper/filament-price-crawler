69e7aa9110b6631614b4f429c5bed79e04beb391
Unrea1 <ghostjumper@protonmail.com>
2025-05-30 22:06:32 +0800

    first things

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from filament-price-crawler!")
+from helium import start_chrome, find_all, kill_browser, Text, S
 
+def get_prices(url: str, price_selector: str) -> str:
+    browser = start_chrome(url, headless=True)
+    try:
+        price_elements = find_all(S(price_selector))
+        if price_elements:
+            for element in price_elements:
+                text = element.web_element.text
+                text_parts = text.split("\n")
+                print(text)
+        else:
+            return "No prices not found"
+    finally:
+        kill_browser()
+
+def main():
+    url = "dasfilament.de/filament-spulen/pla-1-75-mm/"
+    price_selector = ".product--info"  # Change this to the actual CSS selector for the price
+    get_prices(url, price_selector)
 
 if __name__ == "__main__":
     main()
