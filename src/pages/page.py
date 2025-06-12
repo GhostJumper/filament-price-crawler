9340055ccaacece9e2c17d5bd6a6501bdbfb9aff
Unrea1 <ghostjumper@protonmail.com>
2025-06-12 15:14:16 +0000

    adds multi url support

--- src/pages/page.py
+++ src/pages/page.py
@@ -2,7 +2,7 @@ from src.pages.page_config import PageConfig
 
 class Page:
     def __init__(self, page_config: PageConfig):
-        self.url = page_config.url
+        self.urls = page_config.urls
         self.selector = page_config.selector
         self.getname = page_config.get_name
         self.getprice = page_config.get_price
@@ -10,17 +10,17 @@ class Page:
     def get_prices(self) -> list:
         from helium import start_chrome, find_all, kill_browser, S
         import re
-
-        browser = start_chrome(self.url, headless=True)
         prices = []
-        try:
-            price_elements = find_all(S(self.selector))
-            if price_elements:
-                for element in price_elements:
-                    text = element.web_element.text
-                    name = self.getname(text)
-                    price = self.getprice(text)
-                    prices.append({'name': name, 'price': price})
-            return prices
-        finally:
-            kill_browser()
+        for url in self.urls:
+            browser = start_chrome(url, headless=True)
+            try:
+                price_elements = find_all(S(self.selector))
+                if price_elements:
+                    for element in price_elements:
+                        text = element.web_element.text
+                        name = self.getname(text)
+                        price = self.getprice(text)
+                        prices.append({'name': name, 'price': price})
+            finally:
+                kill_browser()
+        return prices
--- src/pages/page_config.py
+++ src/pages/page_config.py
@@ -1,15 +1,18 @@
 from dataclasses import dataclass
 import re
 
+
 @dataclass
 class PageConfig:
-    url: str
+    urls: list[str]
     selector: str
     get_name: callable
     get_price: callable
 
+
 das_filament_config = PageConfig(
-    url="dasfilament.de/filament-spulen/pla-1-75-mm/",
+    urls=["dasfilament.de/filament-spulen/pla-1-75-mm/",
+          "dasfilament.de/filament-spulen/petg-1-75-mm/"],
     selector=".product--info",
     get_name=lambda text: text.split("\n")[0],
     get_price=lambda text: re.findall(r"\d+,\d+ €", text)[1].split()[0],
